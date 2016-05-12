9804d85f48699e3dcb64904d4f5ea5471f9c90f1
vlad <uplight.ca@gmail.com>
2016-05-12 13:58:27 -0400

    server.js

--- server.js
+++ server.js
@@ -4,6 +4,11 @@
 var express = require('express');
 var path = require('path');
 var app = express();
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname + '/myindex.html'))
