5d746d9d065a3e369dffae11e35aea67dac772a2
vlad <uplight.ca@gmail.com>
2016-05-13 23:18:47 -0400

    server.ts

--- server.ts
+++ server.ts
@@ -3,26 +3,33 @@
  */
 /// <reference path="typings/express/express.d.ts" />
 /// <reference path="typings/body-parser/body-parser.d.ts" />
-"use strict";
-var express = require('express');
-var app = express();
-var bodyParser = require('body-parser');
+
+import * as express from 'express';
+
+const app = express();
+
+import * as bodyParser from 'body-parser';
+
 // configure our app to use bodyParser(it let us get the json data from a POST)
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
-app.use(function (req, res, next) {
+app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
-var port = process.env.PORT || 8080;
-var router = express.Router();
+
+const port:number = process.env.PORT || 8080;
+const router = express.Router();
+
 router.get('/', function (req, res) {
-    res.json({ message: 'welcome' });
+    res.json({message: 'welcome'});
 });
+
 // prefixed all routes with /api
 app.use('/api', router);
-app.listen(port, function () {
+
+app.listen(port,function(){   
     console.log('http://127.0.0.1:' + port + '/api');
 });
-//# sourceMappingURL=server.js.map
+
